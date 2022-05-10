cd570e53ef974ef69c0eb551a695a16d2f061b9f
JefVerschuere <versch.jef@gmail.com>
2022-05-10 22:51:04 +0200

    changed table to be white

--- src/components/TwitterResult.jsx
+++ src/components/TwitterResult.jsx
@@ -11,8 +11,8 @@ class TwitterResult extends React.Component {
             <>
             {this.props.isTwitterFetched &&
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-100">
                             <tr>
                                 <th scope="col" className="px-6 py-3">
                                     name
@@ -31,8 +31,8 @@ class TwitterResult extends React.Component {
                         <tbody>
                         {information.result.map((element, i) => {
                                 return (
-                                    <tr key={element.name} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    <tr key={element.name} className="bg-white border-b">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                         {element.name}
                                     </th>
                                     <td className="px-6 py-4">
